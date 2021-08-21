a572e4062dfb25cd32685a821bbe796684a1abca
HAORANLiang <55057580+HAORANLiang@users.noreply.github.com>
2021-08-21 16:10:37 +0800

    删除rest_framework

--- list/urls.py
+++ list/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from rest_framework.routers import DefaultRouter
+
 
 
 from list import views
 
 
-router = DefaultRouter()
 
 urlpatterns = [
     path('add_list/', views.add_list)
