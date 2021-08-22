5e24719a018bf01522c5be241f0a658bf0cbd3e0
Craze-H <70187423+Craze-H@users.noreply.github.com>
2021-08-22 09:42:40 +0800

    message和获取所有个人问卷

--- owner/views.py
+++ owner/views.py
@@ -3,6 +3,7 @@ import json
 from django.db.models import Q
 from django.http import JsonResponse
 from list.models import *
+from list.serializer import ListSerializer
 from django.core.paginator import Paginator
 from question.models import *
 
@@ -10,6 +11,11 @@ from question.models import *
 def get_list(request):
     search_content = request.Get.get("searchContent")
     owner_id = request.Get.get("owner_id")
+    sortType = request.GET.get("sortType")
+    if sortType == 0:
+        list_menu = List.objects.filter(owner_id=owner_id)
+        all_set = ListSerializer(list_menu, many=True)
+        return JsonResponse(all_set.data, safe=False)
     if len(search_content) == 0:
         list_menu = List.objects.filter(owner_id=owner_id)
     else:
@@ -17,7 +23,6 @@ def get_list(request):
             Q(list_name__contains=search_content) &
             Q(owner_id=owner_id)
         )
-    sortType = request.GET.get("sortType")
     if sortType == 1:
         list_menu = list_menu.order_by('-start_time')
     elif sortType == 2:
