f2462985cf06b829dc50348777a63569a4def396
Craze-H <70187423+Craze-H@users.noreply.github.com>
2021-08-22 18:29:35 +0800

    编辑与创建合并

--- list/urls.py
+++ list/urls.py
@@ -5,6 +5,5 @@ from list import views
 urlpatterns = [
     path('add_list', views.add_list),
     path('quest', views.quest),
-    path('set_deadline', views.set_deadline),
     path('set_publish', views.set_publish)
 ]
