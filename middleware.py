de6544f336066336567652dfc71271cd83093e87
HAORANLiang <55057580+HAORANLiang@users.noreply.github.com>
2021-08-22 16:39:53 +0800

    0的权限

--- middleware.py
+++ middleware.py
@@ -6,7 +6,7 @@ from django.http import JsonResponse
 class Verify(MiddlewareMixin):
     def process_request(self, request):
         id = request.headers.get("Authorization")
-        if id != 0:
+        if id != "0":
             tmp = User.objects.filter(user_id=id)
             if not tmp.exists():
                 ret_data = {
