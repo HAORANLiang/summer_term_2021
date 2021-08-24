2d8be86c21a0cbb5ec62da5599b59a501bb6643a
HAORANLiang <55057580+HAORANLiang@users.noreply.github.com>
2021-08-24 08:00:16 +0800

    统计页面增加选项和描述

--- result/views.py
+++ result/views.py
@@ -136,32 +136,33 @@ def statistic(request):
                         x[i] += 1
 
             if question.content_1 != "":
-                tmp = {"1": 0 if (num==0) else int(float(x[0])/num*100)}
+                tmp = {question.content_1: 0 if (num==0) else int(float(x[0])/num*100)}
                 rate.update(tmp)
             if question.content_2 != "":
-                tmp = {"2": 0 if (num==0) else int(float(x[1])/num*100)}
+                tmp = {question.content_2: 0 if (num==0) else int(float(x[1])/num*100)}
                 rate.update(tmp)
             if question.content_3 != "":
-                tmp = {"3": 0 if (num==0) else int(float(x[2])/num*100)}
+                tmp = {question.content_3: 0 if (num==0) else int(float(x[2])/num*100)}
                 rate.update(tmp)
             if question.content_4 != "":
-                tmp = {"4": 0 if (num==0) else int(float(x[3])/num*100)}
+                tmp = {question.content_4: 0 if (num==0) else int(float(x[3])/num*100)}
                 rate.update(tmp)
             if question.content_5 != "":
-                tmp = {"5": 0 if (num==0) else int(float(x[4])/num*100)}
+                tmp = {question.content_5: 0 if (num==0) else int(float(x[4])/num*100)}
                 rate.update(tmp)
             if question.content_6 != "":
-                tmp = {"6": 0 if (num==0) else int(float(x[5])/num*100)}
+                tmp = {question.content_6: 0 if (num==0) else int(float(x[5])/num*100)}
                 rate.update(tmp)
             if question.content_7 != "":
-                tmp = {"7": 0 if (num==0) else int(float(x[6])/num*100)}
+                tmp = {question.content_7: 0 if (num==0) else int(float(x[6])/num*100)}
                 rate.update(tmp)
             if question.content_8 != "":
-                tmp = {"8": 0 if (num==0) else int(float(x[7])/num*100)}
+                tmp = {question.content_8: 0 if (num==0) else int(float(x[7])/num*100)}
                 rate.update(tmp)
             tmp_que = {
                 'no': build.que_no,
                 'title': question.title,
+                'description': question.description,
                 'type': type,
                 'all': num,
                 'all_rate': 0 if (num==0) else int(float(num)/results_num*100),
@@ -200,32 +201,33 @@ def statistic(request):
                     if result.ans8 == i:
                         x[i] += 1
             if question.content_1 != "":
-                tmp = {"1": 0 if (num == 0) else int(float(x[0]) / num * 100)}
+                tmp = {question.content_1: 0 if (num == 0) else int(float(x[0]) / num * 100)}
                 rate.update(tmp)
             if question.content_2 != "":
-                tmp = {"2": 0 if (num == 0) else int(float(x[1]) / num * 100)}
+                tmp = {question.content_2: 0 if (num == 0) else int(float(x[1]) / num * 100)}
                 rate.update(tmp)
             if question.content_3 != "":
-                tmp = {"3": 0 if (num == 0) else int(float(x[2]) / num * 100)}
+                tmp = {question.content_3: 0 if (num == 0) else int(float(x[2]) / num * 100)}
                 rate.update(tmp)
             if question.content_4 != "":
-                tmp = {"4": 0 if (num == 0) else int(float(x[3]) / num * 100)}
+                tmp = {question.content_4: 0 if (num == 0) else int(float(x[3]) / num * 100)}
                 rate.update(tmp)
             if question.content_5 != "":
-                tmp = {"5": 0 if (num == 0) else int(float(x[4]) / num * 100)}
+                tmp = {question.content_5: 0 if (num == 0) else int(float(x[4]) / num * 100)}
                 rate.update(tmp)
             if question.content_6 != "":
-                tmp = {"6": 0 if (num == 0) else int(float(x[5]) / num * 100)}
+                tmp = {question.content_6: 0 if (num == 0) else int(float(x[5]) / num * 100)}
                 rate.update(tmp)
             if question.content_7 != "":
-                tmp = {"7": 0 if (num == 0) else int(float(x[6]) / num * 100)}
+                tmp = {question.content_7: 0 if (num == 0) else int(float(x[6]) / num * 100)}
                 rate.update(tmp)
             if question.content_8 != "":
-                tmp = {"8": 0 if (num == 0) else int(float(x[7]) / num * 100)}
+                tmp = {question.content_8: 0 if (num == 0) else int(float(x[7]) / num * 100)}
                 rate.update(tmp)
             tmp_que = {
                 'no': build.que_no,
                 'title': question.title,
+                'description': question.description,
                 'type': type,
                 'all': num,
                 'all_rate': 0 if (num == 0) else int(float(num) / results_num * 100),
@@ -248,6 +250,7 @@ def statistic(request):
             tmp_que = {
                 'no': build.que_no,
                 'title': question.title,
+                'description': question.description,
                 'type': type,
                 'all': num,
                 'all_rate': 0 if (num==0) else int(float(num)/results_num*100),
@@ -260,6 +263,7 @@ def statistic(request):
             tmp_que = {
                 'no': build.que_no,
                 'title': question.title,
+                'description': question.description,
                 'type': type,
                 'all': num,
                 'all_rate': 0 if (num==0) else int(float(num)/results_num*100),
